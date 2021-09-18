9062521df2efe5e0c7c907d2c0c44404cf6f4c4f
Gromak Maxim <maximgromak@gmail.com>
2021-09-18 15:34:00 +1000

    fixed: task1

--- src/js/GameSavingLoader.js
+++ src/js/GameSavingLoader.js
@@ -1,12 +1,10 @@
 import json from './parser.js';
 import read from './reader.js';
+import GameSaving from './GameSaving.js';
 
 export default class GameSavingLoader {
   static load() {
-    return new Promise((resolve) => {
-      read()
-        .then((response) => json(response))
-        .then((data) => resolve(data));
-    });
+    return read()
+      .then((response) => json(response)).then((r) => new GameSaving(JSON.parse(r)));
   }
 }
